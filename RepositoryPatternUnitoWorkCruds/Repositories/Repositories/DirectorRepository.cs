df1dddd3608879fc07f5c8449da3d09d8ee77293
Felix Jose <felixjoseynfantequezada@gmail.com>
2021-01-18 11:23:09 -0400

    Interface and not implementation yet of movie repository, change method in directorRepository

--- RepositoryPatternUnitoWorkCruds/Repositories/Repositories/DirectorRepository.cs
+++ RepositoryPatternUnitoWorkCruds/Repositories/Repositories/DirectorRepository.cs
@@ -1,11 +1,43 @@
-﻿using System;
+﻿using RepositoryPatternUnitoWorkCruds.Models;
+using RepositoryPatternUnitoWorkCruds.Repositories.IRepositories;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace RepositoryPatternUnitoWorkCruds.Repositories.Repositories
 {
-    public class MovieRepository
+    public class DirectorRepository : IGenericRepository<Director>, IDirectorRepository
     {
+        public void AddGeneric(Director entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteGeneric(Director entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Director> GetAllDirectorByMovieLanguage(string language)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Director> GetAllGeneric()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Director GetByIdGeneric(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public void UpdateGeneric(Director entity)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
