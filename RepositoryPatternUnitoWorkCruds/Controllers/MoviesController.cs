7206b2d44e7d1cc3d7a5dde27c71f471d2c8f26f
Felix Jose <felixjoseynfantequezada@gmail.com>
2021-01-19 21:11:16 -0400

    MoviesController created

---
+++ RepositoryPatternUnitoWorkCruds/Controllers/MoviesController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RepositoryPatternUnitoWorkCruds.Repositories.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RepositoryPatternUnitoWorkCruds.Controllers
+{
+    public class MoviesController : Controller
+    {
+        private readonly UnitOfWork _unitOfWork;
+        public MoviesController(UnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
