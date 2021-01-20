48e8574fdcfff771d6ee8ad7f68268908acaf7a2
Felix Jose <felixjoseynfantequezada@gmail.com>
2021-01-20 00:13:19 -0400

    delete in directorcontroller working

--- RepositoryPatternUnitoWorkCruds/Controllers/DirectorsController.cs
+++ RepositoryPatternUnitoWorkCruds/Controllers/DirectorsController.cs
@@ -37,5 +37,52 @@ namespace RepositoryPatternUnitoWorkCruds.Controllers
             }
             return View(director);
         }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var objectData = _unitOfWork.directorRepositoryGG.GetByIdGeneric(id);
+            if (objectData != null)
+            {
+                return View(objectData);
+            }
+            return NotFound();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> EditPost(Director director)
+        {
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.directorRepositoryGG.UpdateGeneric(director);
+                await _unitOfWork.commit();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(director);
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            var objectData = _unitOfWork.directorRepositoryGG.GetByIdGeneric(id);
+            if (objectData != null)
+            {
+                return View(objectData);
+            }
+            return NotFound();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeletePost(Director director)
+        {
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.directorRepositoryGG.DeleteGeneric(director);
+                await _unitOfWork.commit();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(director);
+        }
     }
 }
